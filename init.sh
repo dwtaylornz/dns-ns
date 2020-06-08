14363f4720e49dc7d67b800876aed2b3e0bb554f
dwtaylornz <32386008+dwtaylornz@users.noreply.github.com>
2020-06-08 16:37:10 +1200

    Update init.sh

--- init.sh
+++ init.sh
@@ -9,11 +9,11 @@ echo POLLING_DELAY = $POLLING_DELAY
 
 # Main loop
 # Delay set to $POLLING_DELAY
+cd /etc/bind
+
 while :
 do
   sleep $POLLING_DELAY
   sed -e "s/\${pub_ip}/210.54.33.24/" -e "s/\${domain}/sigtar.com/" zone.txt > sigtar.com.zone
-  sed -e "s/\${pub_ip}/210.54.33.24/" -e "s/\${domain}/loyalbug.com/" zone.txt > loyalbug.com.zone
-  
-  
+  sed -e "s/\${pub_ip}/210.54.33.24/" -e "s/\${domain}/loyalbug.com/" zone.txt > loyalbug.com.zone  
 done
