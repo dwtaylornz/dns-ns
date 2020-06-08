d4d6145e60993e541e275d09f8e02b80201e259f
dwtaylornz <32386008+dwtaylornz@users.noreply.github.com>
2020-06-08 17:29:17 +1200

    Update init.sh

--- init.sh
+++ init.sh
@@ -7,6 +7,11 @@ fi
 date
 echo POLLING_DELAY = $POLLING_DELAY
 
+#setup
+sed -e "s/\${pub_ip}/210.54.33.24/" -e "s/\${domain}/sigtar.com/" zone.txt > sigtar.com.zone
+sed -e "s/\${pub_ip}/210.54.33.24/" -e "s/\${domain}/loyalbug.com/" zone.txt > loyalbug.com.zone  
+rc-update add named
+
 # Main loop
 # Delay set to $POLLING_DELAY
 cd /etc/bind
