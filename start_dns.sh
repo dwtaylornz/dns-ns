599af8f53d8455946d1f704949957f7511abfa07
dwtaylornz <32386008+dwtaylornz@users.noreply.github.com>
2020-06-08 15:40:11 +1200

    Create start_dns.sh

---
+++ start_dns.sh
@@ -0,0 +1,3 @@
+
+sed -e "s/\${pub_ip}/210.54.33.24/" -e "s/\${domain}/sigtar.com/" /home/root/templates/zone.txt > sigtar.com.zone
+sed -e "s/\${pub_ip}/210.54.33.24/" -e "s/\${domain}/loyalbug.com/" /home/root/templates/zone.txt > loyalbug.com.zone
